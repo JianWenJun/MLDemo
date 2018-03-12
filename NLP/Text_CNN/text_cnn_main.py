230d1f2c7c1200fdea7b5a4ce79241307d498912
JianWenJun <1343483119@qq.com>
2018-03-12 22:11:05 +0800

    add textCnn model

---
+++ NLP/Text_CNN/text_cnn_main.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time : 2018/3/7 下午4:06 
+# @Author : ComeOnJian
+# @File : text_cnn.py
+# implementation of Convolutional Neural Networks for Sentence CLassification
+
+import argparse
+import pandas as pd
+# import ast
+import numpy as np
+# import NLP.Text_CNN.process_data as process_data
+# import NLP.Text_CNN.text_cnn_model as TextCNN
+import process_data
+from text_cnn_model import TextCNN
+import pickle
+from tqdm import tqdm
+
+import pdb
+# step1 get paramater
+# step2 load data
+# step3 create TextCNN model
+# step4 start train
+# step5 validataion
+
+if __name__ == '__main__':
+    # step1 get paramater
+    parse = argparse.ArgumentParser(description='Paramaters for construct TextCNN Model')
+    # #方式一 type = bool
+    # parse.add_argument('--nonstatic',type=ast.literal_eval,help='use textcnn nonstatic or not',dest='tt')
+    # 方式二 取bool值的方式)添加互斥的参数
+    group_static = parse.add_mutually_exclusive_group(required=True)
+    group_static.add_argument('--static', dest='static_flag', action='store_true', help='use static Text_CNN')
+    group_static.add_argument('--nonstatic', dest='static_flag', action='store_false', help='use nonstatic Text_CNN')
+
+    group_word_vec = parse.add_mutually_exclusive_group(required=True)
+    group_word_vec.add_argument('--word2vec', dest='wordvec_flag', action='store_true', help='word_vec is word2vec')
+    group_word_vec.add_argument('--rand', dest='wordvec_flag', action='store_false', help='word_vec is rand')
+
+    group_shuffer_batch = parse.add_mutually_exclusive_group(required=False)
+    group_shuffer_batch.add_argument('--shuffer', dest='shuffer_flag', action='store_true', help='the train do shuffer')
+    group_shuffer_batch.add_argument('--no-shuffer', dest='shuffer_flag', action='store_false',
+                                     help='the train do not shuffer')
+
+    parse.add_argument('--learnrate', type=float, dest='learnrate', help='the NN learnRate', default=0.05)
+    parse.add_argument('--epochs', type=int, dest='epochs', help='the model train epochs', default=2)
+    parse.add_argument('--batch_size', type=int, dest='batch_size', help='the train gd batch size.(50-300)', default=50)
+    parse.add_argument('--dropout_pro', type=float, dest='dropout_pro', help='the nn layer dropout_pro', default=0.5)
+
+    parse.set_defaults(static_flag=True)
+    parse.set_defaults(wordvec_flag=True)
+    parse.set_defaults(shuffer_flag=False)
+
+    args = parse.parse_args()
+
+
+    # step2 load data
+    print('load data. . .')
+    X = pickle.load(open('./NLP/result/word_vec.p','rb'))
+
+    word_vecs_rand, word_vecs, word_cab, sentence_max_len, revs = X[0],X[1],X[2],X[3],X[4]
+
+    print('load data finish. . .')
+    # configuration tf
+    filter_sizes = [3, 4, 5]
+    filter_numbers = 100
+    embedding_size = 300
+    # use word2vec or not
+    W = word_vecs_rand
+    if args.wordvec_flag:
+        W = word_vecs
+        pass
+    # use static train or not
+    static_falg = args.static_flag
+    # use shuffer the data or not
+    shuffer_falg = args.shuffer_flag
+    #交叉验证
+    results = []
+    for index in tqdm(range(10)):
+        #打调试断点
+        # pdb.set_trace()
+        train_x, train_y, test_x, test_y = process_data.get_train_test_data(W,revs,index,sentence_max_len,default_values=0.0,vec_size=300)
+        # step3 create TextCNN model
+        text_cnn = TextCNN(shuffer_falg,static_falg,filter_numbers,filter_sizes,sentence_max_len,embedding_size,args.learnrate,args.epochs,args.batch_size,args.dropout_pro)
+        # step4 start train
+        text_cnn.train(train_x[300:],train_y[300:])
+        # step5 validataion
+        loss,accur = text_cnn.validataion(train_x[0:300], train_y[0:300])
+        #
+        results.append(accur)
+        print('cv {} accur is :{:.3f} loss is {:.3f}'.format(index+1,accur,loss))
+        text_cnn.close()
+    print('last accuracy is {}'.format(np.mean(results)))
+
+
+
---
+++ NLP/Text_CNN/text_cnn_model.py
@@ -0,0 +1,195 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time : 2018/3/9 下午4:35 
+# @Author : ComeOnJian 
+# @File : text_cnn_model.py 
+
+#  structure TextCNN
+#  1.input-embedding layer(max_l * 300) 2.cov layer 3.max pool layer 4.full connect droput + softmax + l2
+
+import tensorflow as tf
+import numpy as np
+import pdb
+
+class TextCNN():
+
+    __shuffer_falg = False
+    __static_falg = True
+
+    def __init__(self,shuffer_falg, static_falg, filter_numbers, filter_sizes, sentence_length,embedding_size,learnrate, epochs, batch_size, dropout_pro):
+
+        self.__shuffer_falg = shuffer_falg
+        self.__static_falg = static_falg
+        self.learning_rate_item = learnrate
+        self.epochs = epochs
+        self.sentence_length = sentence_length
+        self.filter_numbers = filter_numbers
+        self.batch_size = batch_size
+        self.dropout_pro_item = dropout_pro
+        self.embedding_size = embedding_size
+        # 1. setting graph
+        tf.reset_default_graph()
+        self.train_graph = tf.Graph()
+        with self.train_graph.as_default():
+            # self.input_x = tf.placeholder(dtype=tf.float32,shape=[None,sentence_length],name='input_x')
+            # 1 input layer
+
+            if self.__static_falg:
+                # the word vectors are not allowed to change
+                self.embedding_layer = tf.placeholder(dtype=tf.float32,shape=[None,sentence_length,embedding_size],name='embeding_layer')
+                self.embedding_layer_expand = tf.expand_dims(self.embedding_layer,-1)#[None,sentence_length,embedding_size,1]
+            # else:
+            #     self.embedding_layer = tf.Variable(dtype=tf.float32)
+            self.input_y = tf.placeholder(dtype=tf.int32,shape=[None,2],name='input_y')
+            self.dropout_pro = tf.placeholder(dtype=tf.float32,name='dropout_pro')
+            self.learning_rate = tf.placeholder(dtype=tf.float32,name='learning_rate')
+            self.l2_loss = tf.constant(0.0)
+
+            #2 embedding layer
+
+            # with tf.name_scope('word_embedding_layer'):
+            #
+            #     embedding_matrix = tf.Variable()
+            #     tf.expand_dims()
+
+            #3 conv layer + maxpool layer for each filer size
+            pool_layer_lst = []
+            for filter_size in filter_sizes:
+                max_pool_layer = self.__add_conv_layer(filter_size,filter_numbers)
+                pool_layer_lst.append(max_pool_layer)
+
+            # 4.full connect droput + softmax + l2
+            # combine all the max pool —— feature
+
+            with tf.name_scope('dropout_layer'):
+                # pdb.set_trace()
+
+                max_num = len(filter_sizes) * self.filter_numbers
+                h_pool = tf.concat(pool_layer_lst,name='last_pool_layer',axis=3)
+                pool_layer_flat = tf.reshape(h_pool,[-1,max_num],name='pool_layer_flat')
+
+                dropout_pro_layer = tf.nn.dropout(pool_layer_flat,self.dropout_pro,name='dropout')
+
+            with tf.name_scope('soft_max_layer'):
+                SoftMax_W = tf.Variable(tf.truncated_normal([max_num,2],stddev=0.01),name='softmax_linear_weight')
+                self.__variable_summeries(SoftMax_W)
+                # print('test1------------')
+                SoftMax_b = tf.Variable(tf.constant(0.1,shape=[2]),name='softmax_linear_bias')
+                self.__variable_summeries(SoftMax_b)
+                # print('test2------------')
+                self.l2_loss += tf.nn.l2_loss(SoftMax_W)
+                self.l2_loss += tf.nn.l2_loss(SoftMax_b)
+                # dropout_pro_layer_reshape = tf.reshape(dropout_pro_layer,[batch_size,-1])
+                self.softmax_values = tf.nn.xw_plus_b(dropout_pro_layer,SoftMax_W,SoftMax_b,name='soft_values')
+                # print ('++++++',self.softmax_values.shape)
+                self.predictions = tf.argmax(self.softmax_values,axis=1,name='predictions',output_type=tf.int32)
+
+            with tf.name_scope('loss'):
+                losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.softmax_values,labels=self.input_y)
+                self.loss = tf.reduce_mean(losses) + 0.001 * self.l2_loss #lambda = 0.001
+
+                # print ('---------1',self.loss)
+                tf.summary.scalar('last_loss',self.loss)
+
+            with tf.name_scope('accuracy'):
+                correct_acc = tf.equal(self.predictions,tf.argmax(self.input_y,axis=1,output_type=tf.int32))
+
+                self.accuracy = tf.reduce_mean(tf.cast(correct_acc,'float'),name='accuracy')
+                tf.summary.scalar('accuracy',self.accuracy)
+
+            with tf.name_scope('train'):
+                optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+                # print('test1------------')
+                # pdb打个断点
+                # pdb.set_trace()
+                self.train_op = optimizer.minimize(self.loss)
+                # print('test2------------')
+
+            self.session = tf.InteractiveSession(graph=self.train_graph)
+            self.merged = tf.summary.merge_all()
+            self.train_writer = tf.summary.FileWriter('./NLP/log/text_cnn', graph=self.train_graph)
+    def train(self,train_x,train_y):
+        self.session.run(tf.global_variables_initializer())
+
+        #迭代训练
+        for epoch in range(self.epochs):
+            # pdb.set_trace()
+            train_batch = self.__get_batchs(train_x, train_y, self.batch_size)
+            for batch_i in range(len(train_x)//self.batch_size):
+                x,y = next(train_batch)
+                # print('--------',np.array(x).shape)
+                feed = {
+                    self.embedding_layer:x,
+                    self.input_y:y,
+                    self.dropout_pro:self.dropout_pro_item,
+                    self.learning_rate:self.learning_rate_item
+                }
+
+                _,summarys,loss,accuracy = self.session.run([self.train_op,self.merged,self.loss,self.accuracy],feed_dict=feed)
+
+                self.train_writer.add_summary(summarys,epoch)
+                # each 5 batch print log
+                if (batch_i+1) % 15 == 0:
+                    print('Epoch {:>3} Batch {:>4}/{} train_loss = {:.3f} accuracy = {:.3f}'.
+                          format(epoch,batch_i,(len(train_x)//self.batch_size),loss,accuracy))
+
+    def validataion(self,test_x, test_y):
+        test_batch = self.__get_batchs(test_x,test_y,self.batch_size)
+        eval_loss, eval_acc ,count= 0.0, 0.0 ,0
+        for batch_i in range(len(test_x) // self.batch_size):
+            x,y = next(test_batch)
+            feed = {
+                self.embedding_layer: x,
+                self.input_y: y,
+                self.dropout_pro: self.dropout_pro_item,
+                self.learning_rate: self.learning_rate_item
+            }
+            loss ,accuracy = self.session.run([self.loss,self.accuracy],feed_dict=feed)
+            eval_loss ,eval_acc ,count  = eval_acc+accuracy ,eval_acc+loss ,count+1
+            # print('validataion_{}_accuracy is {:.3f}'.format(index,accuracy))
+        return accuracy/count,loss/count
+    def close(self):
+        self.session.close()
+        self.train_writer.close()
+    #generate batch data
+    def __get_batchs(self,Xs,Ys,batch_size):
+        for start in range(0,len(Xs),batch_size):
+            end = min(start+batch_size,len(Xs))
+            yield Xs[start:end],Ys[start:end]
+        pass
+    def __add_conv_layer(self,filter_size,filter_num):
+        with tf.name_scope('conv-maxpool-size%d'%(filter_size)):
+            #convolutio layer
+            filter_shape =[filter_size,self.embedding_size,1,filter_num]
+            W = tf.Variable(tf.truncated_normal(filter_shape,stddev=0.1),name='filter_weight')
+            self.__variable_summeries(W)
+            b = tf.Variable(tf.constant(0.1,shape=[filter_num]),name='filter_bias')
+            self.__variable_summeries(b)
+            #参数说明
+            #第一个参数input：指需要做卷积的输入图像 [训练时一个batch的图片数量, 图片高度, 图片宽度, 图像通道数]
+            #第二个参数filter：相当于CNN中的卷积核 [卷积核的高度，卷积核的宽度，图像通道数，卷积核个数]
+            #第三个参数strides：卷积时在图像每一维的步长，这是一个一维的向量，长度4,
+            #第四个参数padding：string类型的量，只能是"SAME","VALID"其中之一，这个值决定了不同的卷积方式
+            #第五个参数：use_cudnn_on_gpu: bool类型，是否使用cudnn加速，默认为true
+            conv_layer = tf.nn.conv2d(self.embedding_layer_expand,W,strides=[1,1,1,1],padding='VALID',name='conv_layer')
+            relu_layer = tf.nn.relu(tf.nn.bias_add(conv_layer,b),name='relu_layer')
+
+            max_pool_layer = tf.nn.max_pool(relu_layer,ksize=[1,self.sentence_length - filter_size+1,1,1],strides=[1,1,1,1],padding='VALID',name='maxpool')
+            return max_pool_layer
+
+    def __variable_summeries(self,var):
+        """
+        :param var: Tensor, Attach a lot of summaries to a Tensor (for TensorBoard visualization).
+        """
+        with tf.name_scope('summeries'):
+            mean = tf.reduce_mean(var)
+            tf.summary.scalar('mean', mean)  # 记录参数的均值
+
+            with tf.name_scope('stddev'):
+                stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+                tf.summary.scalar('stddev', stddev)
+                tf.summary.scalar('max', tf.reduce_max(var))
+                tf.summary.scalar('min', tf.reduce_min(var))
+
+                # 用直方图记录参数的分布
+                tf.summary.histogram('histogram', var)
